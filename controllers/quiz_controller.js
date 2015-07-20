9c58eefd20f0a515039db0f576729552833aad2c
Tomas Lopez <tlopezv.job@gmail.com>
2015-07-20 21:59:06 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -23,13 +23,13 @@ exports.index = function(req, res) {
   //console.log("search = "+search);
 
   models.Quiz.findAll({where: ["pregunta like ?", search]}).then(function(quizes) {
-    res.render('quizes/index', { quizes: quizes});
+    res.render('quizes/index', { quizes: quizes, errors: []});
   }).catch(function(error) { next(error);})
 };
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  res.render('quizes/show', { quiz: req.quiz});
+  res.render('quizes/show', { quiz: req.quiz, errors: []});
 };
 
 // GET /quizes/:id/answer
@@ -38,7 +38,7 @@ exports.answer = function(req, res) {
   if (req.query.respuesta === req.quiz.respuesta){
     resultado = 'Correcto';
   }
-  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado});
+  res.render('quizes/answer', { quiz: req.quiz, respuesta: resultado, errors: []});
 };
 
 // GET /quizes/new
@@ -47,15 +47,22 @@ exports.new = function(req, res) {
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
 
-  res.render('quizes/new', {quiz: quiz});
+  res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST /quizes/create
 exports.create = function(req, res) {
   var quiz = models.Quiz.build( req.body.quiz );
 
-  // guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  })  // Redirección HTTP (URL relativo) lista de preguntas
+  quiz.validate().then(function(err){
+    if (err) {
+      res.render('quizes/new', {quiz: quiz, errors: err.errors});
+    } else {
+      // guarda en DB los campos pregunta y respuesta de quiz
+      quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+        res.redirect('/quizes');
+      })  // Redirección HTTP (URL relativo) lista de preguntas
+    }
+  });
+
 };
