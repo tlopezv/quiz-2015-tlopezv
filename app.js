6025f7110709dc94d6736676a856fdfccc35bc81
Tomas Lopez <tlopezv.job@gmail.com>
2015-07-27 19:34:33 +0200

    Autenticación y sesión

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+// Se importa el paquete express-session instalado con npm
+var session = require('express-session');
 
 var routes = require('./routes/index');
 // var users = require('./routes/users');
@@ -22,9 +24,25 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+// cookieParser: añadir semilla 'Quiz 2015' para cifrar cookie
+app.use(cookieParser('Quiz 2015'));
+// Instalar MW session
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Helpers dinamicos:
+app.use(function(req, res, next) {
+
+  // guardar path en session.redir para despues de login
+  if (!req.path.match(/\/login|\/logout/)) {
+    req.session.redir = req.path;
+  }
+
+  // Hacer visible req.session en las vistas
+  res.locals.session = req.session;
+  next();
+});
 
 app.use('/', routes);
 // app.use('/users', users);
