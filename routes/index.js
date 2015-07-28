3d87d261309b8a7747282358beea04b03dbbcdfc
Tomas Lopez <tlopezv.job@gmail.com>
2015-07-28 19:36:48 +0200

    Moderación de Comentarios

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res) {
 console.log("Autoload de comandos con :quizId");
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  //autoload :quizId
+router.param('commentId', commentController.load);  //autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -32,6 +33,8 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 
 router.get('/quizes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',     commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+                                            sessionController.loginRequired, commentController.publish);
 
 router.get('/author', function(req, res, next) {
   res.render('author', { title: 'Quiz', errors: [] });
